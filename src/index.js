5bdac8dd16cb9492612fd32acdf84e8fac1e9251
slledru <slledru@msn.com>
2018-01-24 15:53:01 -0700

    display game side by side

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './tictactoc/game';
+import './index.css'
 
 // ========================================
 
-ReactDOM.render(
-  <div>
-  <Game />
-  <Game />
-</div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<ul>
+  <li class="root">
+    <Game/>
+  </li>
+  <li class="root">
+    <Game/>
+  </li>
+</ul>, document.getElementById('root'));
